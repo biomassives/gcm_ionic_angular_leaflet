0c681f467fe2f2539562eba21f1f416b62499db9
G. Willson <greg@biomassiv.es>
2022-03-27 20:17:20 -0600

    Add files via upload

---
+++ settings/settings.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.page.html',
+  styleUrls: ['./settings.page.scss'],
+
+
+})
+export class SettingsPage implements OnInit {
+
+
+
+
+  constructor() { 
+  }
+
+  ngOnInit() {
+  }
+}
